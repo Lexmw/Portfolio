f5d66f0843d7169d2aff8a75f116a7a57124c0bc
lexius <lexius.waltar@gmail.com>
2019-09-18 17:23:38 -0700

    working through some bugs

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+var path = require('path');
+
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.use(express.static('./public'));
+// ?app.use('/contact', express.static('././contact'))
+
+app.get('/', (req, res) =>{
+ res.status(200);
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/thanks', (req, res) => {
+    res.status(200);
+});
+
+module.exports = app;
